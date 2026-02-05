a45fc7690d10e29d7b32930ec8e2ca6510b0aaab
attywenn <wnciplays@gmail.com>
2026-02-05 12:30:09 +0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './styles/App.css'
+import { useState } from 'react'
+
+function App () {
+  return (
+    <>
+      
+    </>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./styles/App.css"
+import App from "./App"
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')).render(
+   <App />
+)
