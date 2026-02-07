9c2da220b677178ee2aeaa05f44957842eb7c982
attywenn <wnciplays@gmail.com>
2026-02-08 04:26:35 +0800

    fix url issue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TheusGSM-Phone-Repair-Services-/',
+  base: '/index.html/',
   plugins: [
     react()
   
