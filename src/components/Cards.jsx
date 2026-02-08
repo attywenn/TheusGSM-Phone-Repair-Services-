fae184e544afa5992d7be110c879b645a9a52e04
attywenn <wnciplays@gmail.com>
2026-02-08 13:48:49 +0800

    logout

---
+++ src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react";
+
+function Cards ({text, role, image}) {
+
+    return (
+        <>
+            <div className="border-solid border-[3px] p-4 m-2 flex max-w-[25em] justify-start items-center rounded-[2em]">
+                <img src={image} alt="image" className="size-[4em] rounded-[50%]" />
+                <div className="flex-column p-[0.4rem] ml-4">
+                    <div className="text-[1.8rem] text-black font-bold">{text}</div>
+                    <div className="text-[1rem] text-black">{role}</div>
+                </div>    
+            </div>
+        
+        </>
+    )
+}
+
+export default Cards;
--- src/features/pages/Landing.jsx
+++ src/features/pages/Landing.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import Header from '../../components/Header.jsx'
 import { useState, useEffect } from "react";
 import Welcome from '../../components/Welcome.jsx'
+import Cards from '../../components/Cards.jsx'
+import Kara from '../../images/kara.webp'
+import Wency from '../../images/wenx.png'
 
 function Landing() {
   return (
     <>
     <div className="flex">
-      <div className="max-w-3xl">
+      <div className="max-w-3xl flex-column">
         <Welcome text="</> Meet the pioneers of phone repair services " speed={40} />
+        <div className="grid grid-cols-2 m-[2.5em]">
+          <Cards text="Wency Herrera" role="Software Developer" image={Wency}/>
+          <Cards text="Wency Herrera" role="Software Developer" image={Wency}/>
+          <Cards text="Wency Herrera" role="Software Developer" image={Wency}/>
+          <Cards text="Wency Herrera" role="Software Developer" image={Wency}/>
+          
+        </div>
       </div>
     </div>  
     </>
